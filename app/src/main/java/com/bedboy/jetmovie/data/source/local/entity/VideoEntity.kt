e7be7c85aae24026d9bc4f8ee2a720c4adbb4024
ufebri <ufebri@live.com>
2021-05-19 18:56:38 +0700

    Delete unacessery parcelable

--- app/src/main/java/com/bedboy/jetmovie/data/source/local/entity/VideoEntity.kt
+++ app/src/main/java/com/bedboy/jetmovie/data/source/local/entity/VideoEntity.kt
@@ -1,9 +1,5 @@
 package com.bedboy.jetmovie.data.source.local.entity
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class VideoEntity(
     var key: String
-) : Parcelable
+)
