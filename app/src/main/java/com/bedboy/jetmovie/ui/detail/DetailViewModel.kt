9755e4422dd9289b9eabcc0902a3f80d1b05bab8
ufebri <ufebri@live.com>
2021-05-27 14:26:36 +0700

    delete unused genre call in detail

--- app/src/main/java/com/bedboy/jetmovie/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/bedboy/jetmovie/ui/detail/DetailViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.bedboy.jetmovie.data.source.DataRepository
 import com.bedboy.jetmovie.data.source.local.entity.DataMovieTVEntity
-import com.bedboy.jetmovie.data.source.local.entity.GenreEntity
 import com.bedboy.jetmovie.data.source.local.entity.VideoEntity
 import com.bedboy.jetmovie.vo.Resource
 
@@ -20,9 +19,6 @@ class DetailViewModel(private val dataRepository: DataRepository) : ViewModel() 
 
     fun getVideos(media_type: String, idData: String): LiveData<Resource<List<VideoEntity>>> =
         dataRepository.getVideoDetail(media_type, idData)
-
-    fun getGenre(media_type: String): LiveData<Resource<List<GenreEntity>>> =
-        dataRepository.getGenre(media_type)
 
     var getDetailTV: LiveData<Resource<DataMovieTVEntity>> =
         Transformations.switchMap(dataID) { mDataID ->
