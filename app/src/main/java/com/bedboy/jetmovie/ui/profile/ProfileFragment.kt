7c3ae2a21fa8bd8cc9bc2b5db4dc2dabb5878dab
Uray Febri Isviladinata <ray@Urays-MacBook-Air.local>
2025-03-08 23:07:26 +0700

    edit: refactor profile

--- app/src/main/java/com/bedboy/jetmovie/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/bedboy/jetmovie/ui/profile/ProfileFragment.kt
@@ -1,17 +1,18 @@
 package com.bedboy.jetmovie.ui.profile
 
 import android.Manifest
-import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CompoundButton
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bedboy.jetmovie.databinding.ContentProfileBinding
-import com.bedboy.jetmovie.utils.PermissionHelper
+import com.bedboy.jetmovie.utils.PermissionManager
 import com.bedboy.jetmovie.utils.ViewModelFactory
 
 class ProfileFragment : Fragment() {
@@ -19,6 +20,12 @@ class ProfileFragment : Fragment() {
     private var contentProfileBinding: ContentProfileBinding? = null
     private val binding get() = contentProfileBinding
     private lateinit var viewModel: ProfileViewModel
+    private lateinit var permissionManager: PermissionManager
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            handlePermissionResult(isGranted)
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,6 +42,8 @@ class ProfileFragment : Fragment() {
         if (activity != null) {
             val factory = ViewModelFactory.getInstance(requireActivity())
             viewModel = ViewModelProvider(this, factory)[ProfileViewModel::class.java]
+
+            permissionManager = PermissionManager(requireActivity())
 
             //Dark Mode Config
             binding?.apply {
@@ -60,18 +69,32 @@ class ProfileFragment : Fragment() {
 
                 switchReminder.setOnCheckedChangeListener { _, isChecked ->
                     if (isChecked) {
-                        if (!PermissionHelper.isPermissionGranted(requireActivity(), Manifest.permission.POST_NOTIFICATIONS)) {
-                            PermissionHelper.requestPermission(requireActivity(), Manifest.permission.POST_NOTIFICATIONS)
-
-                            switchReminder.isChecked = false
-                        } else {
-                            viewModel.setReminder(isChecked)
-                        }
+                        requestOrCheckPermission()
                     } else {
-                        viewModel.setReminder(isChecked)
+                        viewModel.setReminder(false)
                     }
                 }
             }
+        }
+    }
+
+    private fun requestOrCheckPermission() {
+        if (!permissionManager.isPermissionNotificationGranted()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            } else {
+                // Jika Android < 13, tidak perlu meminta izin notifikasi
+                binding?.switchReminder?.isChecked = true
+            }
+        } else {
+            viewModel.setReminder(true)
+        }
+    }
+
+    private fun handlePermissionResult(isGranted: Boolean) {
+        isGranted.let {
+            viewModel.setReminder(it)
+            binding?.switchReminder?.isChecked = it
         }
     }
 
