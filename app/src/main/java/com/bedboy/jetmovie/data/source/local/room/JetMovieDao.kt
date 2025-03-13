27c320ae3dc9f357081945f5f18e1203ba08d197
Uray Febri Isviladinata <ray@Urays-MacBook-Air.local>
2025-03-13 23:51:37 +0700

    add: more unit test

--- app/src/main/java/com/bedboy/jetmovie/data/source/local/room/JetMovieDao.kt
+++ app/src/main/java/com/bedboy/jetmovie/data/source/local/room/JetMovieDao.kt
@@ -3,6 +3,7 @@ package com.bedboy.jetmovie.data.source.local.room
 import androidx.lifecycle.LiveData
 import androidx.paging.DataSource
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
