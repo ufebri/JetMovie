17537187b074a6f6088b4601bb7d4c5804b6c79a
Uray Febri Isviladinata <ray@Urays-MacBook-Air.local>
2025-04-29 21:54:36 +0700

    add: more unit testing

--- app/src/main/java/com/raylabs/jetmovie/utils/DataHelper.kt
+++ app/src/main/java/com/raylabs/jetmovie/utils/DataHelper.kt
@@ -1,6 +1,7 @@
 package com.raylabs.jetmovie.utils
 
 import com.raylabs.jetmovie.data.source.local.entity.GenreEntity
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
@@ -29,14 +30,19 @@ object DataHelper {
 
     fun String.toMillisAt10AM(): Long {
         val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-        val date = dateFormat.parse(this) ?: return 0L
-        val calendar = Calendar.getInstance().apply {
-            time = date
-            set(Calendar.HOUR_OF_DAY, 21)
-            set(Calendar.MINUTE, 15)
-            set(Calendar.SECOND, 0)
-            set(Calendar.MILLISECOND, 0)
+        return try {
+            val date = dateFormat.parse(this) ?: return 0L
+            val calendar = Calendar.getInstance().apply {
+                time = date
+                set(Calendar.HOUR_OF_DAY, 10)  // Set to 10:00 AM
+                set(Calendar.MINUTE, 0)
+                set(Calendar.SECOND, 0)
+                set(Calendar.MILLISECOND, 0)
+            }
+            calendar.timeInMillis
+        } catch (e: ParseException) {
+            // Return 0L if the date is invalid or parsing fails
+            0L
         }
-        return calendar.timeInMillis
     }
 }
